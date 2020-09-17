69c32a9f9d9a421d9ae7288fd25588e4d3782585
jrgenhalse <jorgevh@stud.ntnu.no>
2020-09-17 13:36:16 +0200

    feat: legge til kontrollere for løp og løpliste

---
+++ gazelleFX/src/gazelle/ui/CourseController.java
@@ -0,0 +1,24 @@
+package gazelle.ui;
+
+import gazelle.model.Course;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+import java.io.IOException;
+
+public class CourseController extends BaseController {
+
+    private Course course;
+
+    @FXML
+    private Text courseTitle;
+
+    public void setCourse(Course course) {
+        this.course = course;
+        courseTitle.setText(course.getName());
+    }
+
+    public static CourseController load() throws IOException {
+        return loadFromFXML("/scenes/course.fxml");
+    }
+}
