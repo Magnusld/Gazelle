f11c3cf38afe2721ff438137d6342f45f2d61e09
haved <krogstie.havard@gmail.com>
2020-09-17 19:56:10 +0200

    test: legge til tester av databaser på kryss

---
+++ gazelleFX/test/gazelle/model/DatabaseTest.java
@@ -0,0 +1,34 @@
+package gazelle.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DatabaseTest {
+
+    @Test
+    private void databaseIdTest() {
+        Database db = new Database();
+        Course c1 = db.newCourse("Hei");
+        User u1 = db.newUser();
+        User u2 = db.newUser();
+
+        assertTrue(db.getCourse(c1.getId()) == c1);
+        assertTrue(db.getUser(u1.getId()) == u1);
+        assertTrue(db.getUser(u2.getId()) == u2);
+        assertTrue(db.getUser(c1.getId()) == null);
+    }
+
+    @Test
+    private void databaseCrossTest() {
+        Database db = new Database();
+        Course c1 = db.newCourse("Hei");
+        assertTrue(c1.getDatabase() == db);
+
+        Database db2 = new Database();
+        User u1 = db2.newUser();
+
+        assertThrows(IllegalArgumentException.class, ()->u1.addAsOwner(c1));
+        assertThrows(IllegalArgumentException.class, ()->c1.addOwner(u1));
+    }
+}
