e09d5b4bae5ee18237897e3560146cee78e567e6
haved <krogstie.havard@gmail.com>
2020-10-01 00:47:16 +0200

    build: gjøre start.sh ekstremt mye finere

--- start.sh
+++ start.sh
@@ -8,16 +8,12 @@ mvn spring-boot:start -pl server
 mvn javafx:run -pl gazelleFX & pid=$!
 
 # In case the user wants to Ctrl-C out of the script
-# - we kill the javafx:run parent process
-# - we find the java process for GazelleFX
-# - kill it
+# - we kill the javafx:run parent process and all its children
 # - we kindly ask the server to shutdown
 trap "\
-kill $pid
-client=\$(jps -l | grep gazelle.ui.GazelleFX | awk '{print \$1}' | head -n 1)
-kill \$client
-curl -X POST localhost:8080/actuator/shutdown
-sleep 2" SIGINT
+kill $pid \$(ps -o pid= --ppid $pid)
+curl -X POST localhost:8080/actuator/shutdown\
+" SIGINT
 
 wait $pid
 
