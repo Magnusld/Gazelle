58172193e1d5c7bd71daea78a7bfd3103de388ab
Magnus Lykke Dahlø <magnusld@stud.ntnu.no>
2020-10-12 08:25:26 +0200

    test(UserTest): legge til flere bruke tester

--- server/test/gazelle/server/UserTest.java
+++ server/test/gazelle/server/UserTest.java
@@ -1,17 +1,16 @@
 package gazelle.server;
 
+import gazelle.auth.LogInRequest;
 import gazelle.auth.LogInResponse;
 import gazelle.auth.SignUpRequest;
 import gazelle.model.User;
 import gazelle.server.endpoint.LoginEndpoint;
+import gazelle.server.error.LoginFailedException;
 import gazelle.server.repository.UserRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 import java.util.Optional;
 
@@ -25,14 +24,31 @@ public class UserTest {
     @Autowired
     private UserRepository userRepository;
 
-    private LoginEndpoint loginEndpoint = new LoginEndpoint(userRepository);
+    @Autowired
+    private LoginEndpoint loginEndpoint;
 
     @Test
     public void signUpNewUserTest() {
-        userRepository.save(loginEndpoint.signup(new SignUpRequest("niss2","niss2")).getUser());
+        if (userRepository.findByUsername("niss2").isPresent()) {
+            userRepository.delete(userRepository.findByUsername("niss2").get());
+        }
+        SignUpRequest signUpRequest = new SignUpRequest("niss2", "niss2");
+        loginEndpoint.signup(signUpRequest);
         assertTrue(userRepository.findByUsername("niss2").isPresent());
-
+        userRepository.delete(userRepository.findByUsername("niss2").get());
     }
+
+    @Test
+    public void logInUserTest() {
+        User user = userRepository.save(new User("niss3","niss3"));
+        LogInRequest logInRequest = new LogInRequest("niss3","niss3");
+        LogInResponse logInResponse = loginEndpoint.login(logInRequest);
+        assertTrue(logInResponse.getToken().equals("dummy-token"));
+        Throwable exception = assertThrows(LoginFailedException.class,
+                () -> loginEndpoint.login(new LogInRequest("nise", "nise")));
+        System.out.println(exception.getMessage());
+    }
+
 
     @Test
     public void userRepositoryTest() {
@@ -41,14 +57,11 @@ public class UserTest {
         Optional<User> foundUser = userRepository.findByUsername("niss1");
         assertTrue(foundUser.isPresent());
         assertTrue(user.getId().equals(userRepository.findByUsername("niss1")
-                .stream().findAny().get().getId()));
+                .get().getId()));
+        assertTrue(userRepository.findById(user.getId()).isPresent());
         userRepository.delete(user);
     }
 
-    @Test
-    public void logInUserTest() {
-
-    }
 
     @Test
     public void testDatabaseTest() {
