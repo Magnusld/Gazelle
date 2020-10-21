24215f4a917c31f8a31ecace1ab2ee7bf0a2c0d6
Lars Tønder <larstond@stud.ntnu.no>
2020-10-21 12:13:09 +0200

    feat(#78): legge til løpsliste-komponent og typer

---
+++ gazelle/src/components/CourseList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="courseList">
+    <h1>Mine løp</h1>
+    <span id="separator"></span>
+    <div class="courses">
+      <Course v-for="(course, index) in courses" :key="index" :course="course" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import type { Course } from "@/types"
+
+@Options({
+  props: {
+    courses: [Course]
+  }
+})
+export default class CourseList extends Vue {
+  courses!: Course[];
+}
+</script>
+
+<style scoped>
+
+</style>
