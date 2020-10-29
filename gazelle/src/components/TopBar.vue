82a70ccc57175acfe7d8573228ad9e52f0ec23eb
Lars Tønder <larstond@stud.ntnu.no>
2020-10-29 13:30:18 +0100

    feat(#104): gjøre om app-ikonet til en svg-fil og knapp på nettsida

--- gazelle/src/components/TopBar.vue
+++ gazelle/src/components/TopBar.vue
@@ -1,15 +1,17 @@
 <template>
     <md-content class="md-layout md-primary md-elevation-2" id="topBar">
       <div class="md-layout-item md-layout">
-        <router-link to="/">
-          <img src="../assets/logo.png" alt="logo" id="navLogo"/>
-        </router-link>
+        <div class="buttonContainer">
+          <md-button class="md-icon-button" to="/">
+            <img src="../assets/applogo.svg" alt="applogo">
+          </md-button>
+        </div>
         <md-tabs class="md-primary" id="navTabs" md-sync-route>
           <md-tab md-label="Mine løp" to="/my-courses"></md-tab>
           <md-tab md-label="Fokuslista" to="/focus-list"></md-tab>
         </md-tabs>
       </div>
-      <div class="md-layout-item" id="buttonContainer">
+      <div class="md-layout-item buttonContainer">
         <md-button class="md-icon-button" id="userInfo" to="/user-settings">
           <md-icon>person</md-icon>
         </md-button>
@@ -34,7 +36,7 @@ export default class TopBar extends Vue {
   height: 45px;
   padding: 5px;
 }
-#buttonContainer {
+.buttonContainer {
   display: flex;
   justify-content: flex-end;
   align-items: center;
