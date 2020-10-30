e92f7c443dc73e471306fb9a8e1e0d22550674dc
jrgenhalse <jorgevh@stud.ntnu.no>
2020-10-30 14:28:18 +0100

    feat(#106): legge til markdown-støtte

--- gazelle/src/main.ts
+++ gazelle/src/main.ts
@@ -5,11 +5,15 @@ import store from "./store";
 import VueMaterial from "vue-material";
 import "vue-material/dist/vue-material.min.css";
 import "vue-material/dist/theme/default.css";
+import VueMarkdown from 'vue-markdown';
 
 Vue.config.productionTip = false;
 Vue.use(VueMaterial);
 
 new Vue({
+  components: {
+    'vue-markdown': VueMarkdown
+  },
   router,
   store,
   render: h => h(App)
