60be7c2c1ffd60695f77b53df18387b0d25c63c4
jrgenhalse <jorgevh@stud.ntnu.no>
2020-11-02 13:36:42 +0100

    feat(#106): legge til markdown

--- gazelle/src/views/FocusList.vue
+++ gazelle/src/views/FocusList.vue
@@ -1,12 +1,21 @@
 <template>
   <div class="focusList">
     Fokuslista
+    <vue-markdown>
+      Litt *deilig* **markdown**
+    </vue-markdown>
   </div>
+
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import VueMarkdown from "vue-markdown";
 
-@Component
+@Component({
+  components: {
+    'vue-markdown': VueMarkdown
+  }
+})
 export default class Home extends Vue {}
 </script>
