d81e0feb6a3927252427860e7a12d76cb173f302
jrgenhalse <jorgevh@stud.ntnu.no>
2020-11-05 03:44:43 +0100

    test(#118): legge til starten på testmetode for addCoruseOwner

--- server/test/gazelle/server/endpoint/CourseOwnControllerTest.java
+++ server/test/gazelle/server/endpoint/CourseOwnControllerTest.java
@@ -3,6 +3,9 @@ package gazelle.server.endpoint;
 import gazelle.model.Course;
 import gazelle.model.User;
 import gazelle.server.TestHelper;
+import gazelle.server.error.AuthorizationException;
+import gazelle.server.error.InvalidTokenException;
+import gazelle.server.error.MissingAuthorizationException;
 import gazelle.server.service.CourseAndUserService;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -26,7 +29,9 @@ public class CourseOwnControllerTest {
     private Course course1;
     private Course course2;
     private User user1;
-    private User user2
+    private User user2;
+    private String token1;
+    private String token2;
 
     @BeforeAll
     public void setup() {
@@ -34,6 +39,8 @@ public class CourseOwnControllerTest {
         course2 = testHelper.createTestCourseObject();
         user1 = testHelper.createTestUserObject();
         user2 = testHelper.createTestUserObject();
+        token1 = testHelper.logInUser(user1.getId());
+        token2 = testHelper.logInUser(user2.getId());
     }
 
     @Test
@@ -58,5 +65,30 @@ public class CourseOwnControllerTest {
         assertTrue(owners.contains(user1));
         assertTrue(owners.contains(user2));
         assertFalse(owners.contains(user3));
+    }
+
+    /**
+     * Støtt på eit problem med denne metoden sidan eg må ta inn ein authorization token
+     * Korleis?
+     */
+    @Test
+    public void addCourseOwner() {
+        courseAndUserService.addOwner(user1, course1);
+        assertThrows(MissingAuthorizationException.class, () -> {
+            courseOwnController.addCourseOwner(user2.getId(), course1.getId(), null);
+        });
+        // Spørsmålet er no, kva er ein invalid token?
+        assertThrows(InvalidTokenException.class, )
+        assertThrows(AuthorizationException.class, () -> {
+            courseOwnController.addCourseOwner(user2.getId(), course1.getId(), token2);
+        });
+
+        courseOwnController.addCourseOwner(user2.getId(), course1.getId(), token1); // Antek at det er tokenen til den som skal ha authorization som må vere her?
+
+    }
+
+    @Test
+    public void addRedundantCourseOwner() {
+
     }
 }
