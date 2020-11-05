312bb5d9fc90af7a1bcf597b4eb71efb9b4ccd19
jrgenhalse <jorgevh@stud.ntnu.no>
2020-11-05 03:19:02 +0100

    test(#118): lage testklassen og første testmetode

---
+++ server/test/gazelle/server/endpoint/CourseOwnControllerTest.java
@@ -0,0 +1,49 @@
+package gazelle.server.endpoint;
+
+import gazelle.model.Course;
+import gazelle.model.User;
+import gazelle.server.TestHelper;
+import gazelle.server.service.CourseAndUserService;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CourseOwnControllerTest {
+
+    @Autowired
+    private CourseOwnController courseOwnController;
+
+    @Autowired
+    private TestHelper testHelper;
+
+    @Autowired
+    private CourseAndUserService courseAndUserService;
+
+    private Course course1;
+    private Course course2;
+    private User user1;
+    private User user2
+
+    @BeforeAll
+    public void setup() {
+        course1 = testHelper.createTestCourseObject();
+        course2 = testHelper.createTestCourseObject();
+        user1 = testHelper.createTestUserObject();
+        user2 = testHelper.createTestUserObject();
+    }
+
+    @Test
+    public void getOwnedCourses() {
+        courseAndUserService.addOwner(user1, course1);
+        courseAndUserService.addOwner(user1, course2);
+        ArrayList<Course> courses;
+        courses = courseOwnController.getOwnedCourses(user1.getId());
+        assertEquals(2, courses.size());
+        assertTrue(courses.contains(course1));
+        assertTrue(courses.contains(course2));
+    }
+}
