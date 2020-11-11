914f85e4c9c846468beb3d903524950dc1a521ce
jrgenhalse <jorgevh@stud.ntnu.no>
2020-11-11 14:40:11 +0100

    feat(#134): endre namn frå task til chore

---
+++ gazelle/src/components/FocusChore.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="subheader">{{chore.courseName}}</div>
+    <md-checkbox v-model="checked" class="checkbox">{{chore.name}}</md-checkbox>
+    <md-divider/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class FocusChore extends Vue {
+  @Prop()
+  private chore!: { name: string, courseName: string, courseId: number, postName: string, postId: number };
+  private checked = false;
+}
+
+</script>
+
+<style scoped>
+
+.subheader {
+  color: gray;
+}
+.checkbox {
+  --md-theme-default-accent: #F29253
+}
+
+</style>
--- gazelle/src/views/FocusList.vue
+++ gazelle/src/views/FocusList.vue
@@ -1,15 +1,46 @@
 <template>
   <div class="focusList">
-    Fokuslista
-    <vue-markdown>
-      Litt *deilig* **markdown**
-    </vue-markdown>
+    <div class="horizontalSeparator">
+      <span class="md-headline">Fokuslista</span>
+      <md-button class="md-icon-button">
+        <md-icon>delete</md-icon>
+      </md-button>
+    </div>
+    <FocusChore>
+      v-for="(course, index) in courses"
+      :key="index"
+      :course="course"
+    </FocusChore>
+    <md-divider />
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import {Component, Prop, Vue} from "vue-property-decorator";
+import FocusChore from "@/components/FocusChore.vue";
+import {Chore} from "@/types";
 
-@Component
-export default class FocusList extends Vue {}
+@Component({
+  components: {
+    FocusChore
+  }
+})
+export default class FocusList extends Vue {
+  @Prop()
+  private chores!: Chore[];
+}
+
 </script>
+
+<style>
+.horizontalSeparator {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 0 10px 10px;
+}
+.focusList {
+  margin: auto;
+  width: 60%;
+}
+</style>
