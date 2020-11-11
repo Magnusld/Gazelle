0c4aafb9c4fc48a9e580d300ae70c1f4636dced4
haved <krogstie.havard@gmail.com>
2020-11-11 12:25:25 +0100

    style(#141): fikse linting

--- gazelle/vue.config.js
+++ gazelle/vue.config.js
@@ -1,10 +1,9 @@
-
 module.exports = {
-    configureWebpack: {
-        devServer: {
-            host: '0.0.0.0',
-            port: 8080,
-            disableHostCheck: true
-        }
+  configureWebpack: {
+    devServer: {
+      host: "0.0.0.0",
+      port: 8080,
+      disableHostCheck: true
     }
-}
+  }
+};
