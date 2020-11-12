950c34489b7bdbe893b7ef48e4de9d3a6e318493
Lars Tønder <larstond@stud.ntnu.no>
2020-11-12 12:10:05 +0100

    fix(#145): kjøre yarn lint

--- gazelle/src/components/PostChore.vue
+++ gazelle/src/components/PostChore.vue
@@ -10,7 +10,7 @@
     >
       <md-icon>event_available</md-icon>
       <md-tooltip md-direction="right">
-        {{isFocused ? "Fjern fra fokusliste" : "Legg til i fokusliste"}}
+        {{ isFocused ? "Fjern fra fokusliste" : "Legg til i fokusliste" }}
       </md-tooltip>
     </md-button>
   </div>
