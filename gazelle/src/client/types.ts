285b3a2af6adbe9e4d88f0b83d1259618d50790e
Lars Tønder <larstond@stud.ntnu.no>
2020-11-15 15:29:44 +0100

    feat(#153): legge til typer for løps- og postresponser

--- gazelle/src/client/types.ts
+++ gazelle/src/client/types.ts
@@ -16,3 +16,30 @@ export interface LogInResponse {
   user: User;
   token: string;
 }
+
+export interface PostResponse {
+  id: number;
+  title: string;
+  startDate: Date;
+  endDate: Date;
+  choresDone: number;
+  choresFocused: number;
+  choresCount: number;
+}
+
+export interface PostRequest {
+
+}
+
+export interface CourseRequest {
+
+}
+
+export interface CourseResponse {
+  id: number;
+  name: string;
+  isOwner: boolean;
+  isFollower: boolean;
+  currentPost: PostResponse;
+  nextPost: PostResponse;
+}
