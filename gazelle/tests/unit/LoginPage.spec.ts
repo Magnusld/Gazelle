64b24f1807536d239b06b1d884891d402c3a9fc7
Magnus Lykke Dahlø <magnusld@stud.ntnu.no>
2020-11-17 13:20:15 +0100

    feat(#157):Legge til LoginPage test

---
+++ gazelle/tests/unit/LoginPage.spec.ts
@@ -0,0 +1,13 @@
+import { createLocalVue, shallowMount } from "@vue/test-utils";
+import VueMaterial from "vue-material";
+
+
+const localVue = createLocalVue()
+localVue.use(VueMaterial)
+
+describe('LoginPage.vue', () => {
+    it('', () => {
+        expect(true)
+    })
+})
+
