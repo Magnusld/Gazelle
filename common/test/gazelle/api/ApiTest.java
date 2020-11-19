ea8479c731aa532ed3b699a99e1267135e165c36
jrgenhalse <jorgevh@stud.ntnu.no>
2020-11-19 23:08:43 +0100

    test(#182): fullføre testing av common.gazelle.api

--- common/test/gazelle/api/ApiTest.java
+++ common/test/gazelle/api/ApiTest.java
@@ -2,6 +2,7 @@ package gazelle.api;
 
 import gazelle.model.Chore;
 import gazelle.model.UserChoreProgress;
+import net.bytebuddy.asm.Advice;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -16,8 +17,11 @@ public class ApiTest {
     @Test
     public void choreFullResponse() {
         ChoreFullResponse.Builder builder = new ChoreFullResponse.Builder();
+        LocalDate dueDate = LocalDate.now();
+        UserChoreProgress.Progress progress = UserChoreProgress.Progress.FOCUSED;
         builder
                 .id(1L).key(2L).text("Gjøre øving 1")
+                .dueDate(dueDate).progress(progress)
                 .postId(1L).postTitle("Forelesning 1")
                 .courseId(1L).courseName("It-prosjekt");
         ChoreFullResponse choreFullResponse = builder.build();
@@ -122,4 +126,110 @@ public class ApiTest {
         assertEquals(newChoreRequest,newChoreRequest);
     }
 
+    @Test
+    public void newCourseRequest() {
+        String name = "test";
+        NewCourseRequest newCourseRequest = new NewCourseRequest(name);
+
+        assertEquals(name, newCourseRequest.getName());
+        newCourseRequest.setName("test2");
+        assertNotEquals(name, newCourseRequest.getName());
+        assertEquals(newCourseRequest.getName(), "test2");
+
+        assertEquals(newCourseRequest, newCourseRequest);
+    }
+
+    @Test
+    public void newPostRequest() {
+        NewPostRequest.Builder builder = new NewPostRequest.Builder();
+        List<NewChoreRequest> chores = new ArrayList<>();
+        LocalDate startDate = LocalDate.now();
+        LocalDate endDate = LocalDate.now().plusWeeks(2);
+        builder
+                .title("test").description("test description")
+                .startDate(startDate).endDate(endDate)
+                .chores(chores);
+        NewPostRequest newPostRequest = builder.build();
+
+        assertEquals("test", newPostRequest.getTitle());
+        assertEquals("test description", newPostRequest.getDescription());
+        assertEquals(startDate, newPostRequest.getStartDate());
+        assertEquals(endDate, newPostRequest.getEndDate());
+        assertEquals(chores, newPostRequest.getChores());
+
+        assertEquals(newPostRequest, newPostRequest);
+    }
+
+    @Test
+    public void postContentResponse() {
+        PostContentResponse.Builder builder = new PostContentResponse.Builder();
+        LocalDate startDate = LocalDate.now();
+        LocalDate endDate = LocalDate.now().plusWeeks(2);
+        List<ChoreResponse> chores = new ArrayList<>();
+        builder
+                .id(1L).title("test").description("test description")
+                .startDate(startDate).endDate(endDate)
+                .courseId(11L).courseName("courseTest").isOwning(false).chores(chores);
+        PostContentResponse postContentResponse = builder.build();
+
+        assertEquals(1L, postContentResponse.getId());
+        assertEquals("test", postContentResponse.getTitle());
+        assertEquals("test description", postContentResponse.getDescription());
+        assertEquals(startDate, postContentResponse.getStartDate());
+        assertEquals(endDate, postContentResponse.getEndDate());
+        assertEquals(11L, postContentResponse.getCourseId());
+        assertEquals("courseTest",postContentResponse.getCourseName());
+        assertEquals(false, postContentResponse.getIsOwning());
+        assertEquals(chores, postContentResponse.getChores());
+
+        assertEquals(postContentResponse, postContentResponse);
+    }
+
+    @Test
+    public void postResponse() {
+        PostResponse.Builder builder = new PostResponse.Builder();
+        LocalDate startDate = LocalDate.now();
+        LocalDate endDate = LocalDate.now().plusWeeks(2);
+        ChoreResponse nextChoreDue = new ChoreResponse();
+        builder
+                .id(1L).title("test").description("test description")
+                .startDate(startDate).endDate(endDate)
+                .nextChoreDue(nextChoreDue).choresDone(10).choresFocused(3).choresCount(5);
+        PostResponse postResponse = builder.build();
+
+        assertEquals(1L, postResponse.getId());
+        assertEquals("test", postResponse.getTitle());
+        assertEquals("test description", postResponse.getDescription());
+        assertEquals(startDate, postResponse.getStartDate());
+        assertEquals(endDate, postResponse.getEndDate());
+        assertEquals(nextChoreDue, postResponse.getNextChoreDue());
+        assertEquals(10, postResponse.getChoresDone());
+        assertEquals(3, postResponse.getChoresFocused());
+        assertEquals(5, postResponse.getChoresCount());
+
+        assertEquals(postResponse, postResponse);
+    }
+
+    @Test
+    public void userResponse() {
+        UserResponse.Builder builder = new UserResponse.Builder();
+        builder.id(1L).firstName("Per").lastName("Johansen");
+        UserResponse userResponse = builder.build();
+
+        assertEquals(1L, userResponse.getId());
+        assertEquals("Per", userResponse.getFirstName());
+        assertEquals("Johansen", userResponse.getLastName());
+
+        assertEquals(userResponse, userResponse);
+        assertFalse(userResponse.equals((int) 3));
+    }
+
+    @Test
+    public void valueWrapper() {
+        ValueWrapper valueWrapper = new ValueWrapper(3);
+        assertEquals(3, valueWrapper.getValue());
+        valueWrapper.setValue("string");
+        assertNotEquals(3, valueWrapper.getValue());
+        assertEquals("string", valueWrapper.getValue());
+    }
 }
