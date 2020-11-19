d3cffbc5179493a0c98f551be12b0f3e144ab5e8
Lars Tønder <larstond@stud.ntnu.no>
2020-11-19 15:29:58 +0100

    feat(#183): skjule tom status ved henting av data

--- gazelle/src/components/CourseList.vue
+++ gazelle/src/components/CourseList.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="horizontalSeparator" v-if="courses && courses.length > 0">
+    <div class="horizontalSeparator">
       <span class="md-headline">{{ owner ? "Mine løp" : "Fulgte løp" }}</span>
       <div v-if="owner" class="centeredButton">
         <md-button class="md-icon-button" @click="showNewCourseDialog">
@@ -12,7 +12,7 @@
         </md-button>
       </div>
     </div>
-    <div class="courses">
+    <div class="courses" v-if="courses">
       <CourseListing
         v-for="course in courses"
         :key="course.id"
@@ -62,7 +62,7 @@ import { addNewCourse, deleteCourse } from "@/client/course";
   components: { CourseListing }
 })
 export default class CourseList extends Vue {
-  @Prop({ default: null }) private courses!: CourseResponse[];
+  @Prop({ default: null }) private courses!: CourseResponse[] | null;
   @Prop({ default: false }) private owner!: boolean;
 
   private showDialog = false;
