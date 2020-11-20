fae959818207768f36b14499e8cac5fbbb3a9821
jrgenhalse <jorgevh@stud.ntnu.no>
2020-11-20 16:26:27 +0100

    chore(#194): slette alle doble klasser

--- gazelleFX/src/gazelle/client/error/LogInException 2.java
+++
@@ -1,4 +0,0 @@
-package gazelle.client.error;
-
-public class LogInException extends RuntimeException {
-}
--- gazelleFX/src/gazelle/client/error/SignUpException 2.java
+++
@@ -1,7 +0,0 @@
-package gazelle.client.error;
-
-public class SignUpException extends RuntimeException {
-    public SignUpException(String message) {
-        super(message);
-    }
-}
