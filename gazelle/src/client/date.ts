a00c03dde806e84353e39b703dba3f53aedf66d1
Lars Tønder <larstond@stud.ntnu.no>
2020-11-20 15:37:20 +0100

    feat(#170): sørge for at dato sendes riktig til server

--- gazelle/src/client/date.ts
+++ gazelle/src/client/date.ts
@@ -1,9 +1,22 @@
-export type LocalDate = string;
+export type LocalDate = [number, number, number];
 
-export function localDateOfDate(date: Date) {
-  return date.toISOString().split("T")[0];
+export function localDateOfDate(date: Date): LocalDate {
+  const nums = date
+    .toISOString()
+    .split("T")[0]
+    .split("-")
+    .map(Number);
+  return [nums[0], nums[1], nums[2]];
 }
 
-export function dateOfLocalDate(localDate: LocalDate) {
-  return localDate + "T23:00:00.000Z";
+export function dateOfLocalDate(localDate: LocalDate): Date {
+  return new Date(
+    `${localDate[0]}-${localDate[1]}-${localDate[2]}T23:00:00.000Z`
+  );
+}
+
+export function daysUntil(localDate: LocalDate): number {
+  return Math.floor(
+    (dateOfLocalDate(localDate).getTime() - new Date().getTime()) / 86400000
+  );
 }
