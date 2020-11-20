c2ea567f9bae7c7c4ad36e314572e515ecbf98dc
Lars Tønder <larstond@stud.ntnu.no>
2020-11-20 17:23:39 +0100

    feat(#195): legge til fokusliste og gjøremål

--- gazelleFX/src/gazelle/ui/FocusListController.java
+++ gazelleFX/src/gazelle/ui/FocusListController.java
@@ -1,8 +1,11 @@
 package gazelle.ui;
 
+import gazelle.api.ChoreFullResponse;
 import gazelle.client.error.ClientException;
 import javafx.fxml.FXML;
 import javafx.scene.layout.VBox;
+
+import java.util.List;
 
 public class FocusListController extends BaseController {
 
@@ -10,6 +13,7 @@ public class FocusListController extends BaseController {
     private VBox list;
 
     private final GazelleController app;
+
 
     private FocusListController(GazelleController app) {
         this.app = app;
@@ -19,11 +23,24 @@ public class FocusListController extends BaseController {
         list.getChildren().clear();
         app.sideRun(() -> {
             try {
-                app.getClient();
+                List<ChoreFullResponse> chores = app.getClient().chores()
+                        .getFocusedChores(app.getClient().loggedInUserId());
+                app.mainRun(() -> {
+                    for (ChoreFullResponse chore : chores) {
+                        FocusChoreController fcc = FocusChoreController.load(app);
+                        fcc.setChore(chore);
+                        list.getChildren().add(fcc.getNode());
+                    }
+                });
             } catch (ClientException e) {
-                FxUtils.showAndWaitError("Klarte ikke laste fokuslista", e.getMessage());
+                app.mainRun(() -> {
+                    FxUtils.showAndWaitError("Klarte ikke laste fokuslista", e.getMessage());
+                    app.showMyCourses();
+                });
             }
         });
+
+
     }
 
     public static FocusListController load(GazelleController app) {
